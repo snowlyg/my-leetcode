b14ccc87f69fd4528bbf827b8a15742f4b935eb8
snowlyg <569616226@qq.com>
2022-07-04 11:00:23 +0800

    每日两题

---
+++ 28.实现-str-str.go
@@ -0,0 +1,35 @@
+//go:build 28
+// +build 28
+
+package main
+
+func main() {
+	haystack := "mississippi"
+	needle := "mississippi"
+
+	println(strStr(haystack, needle))
+}
+
+/*
+ * @lc app=leetcode.cn id=28 lang=golang
+ *
+ * [28] 实现 strStr()
+ */
+
+// @lc code=start
+func strStr(haystack string, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+	if len(haystack) == 0 {
+		return -1
+	}
+	for i := 0; i <= len(haystack)-len(needle); i++ {
+		if haystack[i:i+len(needle)] == needle {
+			return i
+		}
+	}
+	return -1
+}
+
+// @lc code=end
