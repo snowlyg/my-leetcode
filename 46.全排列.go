e596bd17c79b7ab21e145ec1db4db5bd655f21c4
snowlyg <569616226@qq.com>
2022-07-05 16:21:13 +0800

    [46] 全排列

---
+++ 46.全排列.go
@@ -0,0 +1,46 @@
+package main
+
+func main() {
+	nums := []int{1, 2, 3}
+	p := permute(nums)
+	if p != nil {
+		for i := 0; i < len(nums); i++ {
+			if p[i] == nil {
+				continue
+			}
+			for _, v := range p[i] {
+				println(v)
+			}
+			println("================")
+		}
+	}
+}
+
+/*
+ * @lc app=leetcode.cn id=46 lang=golang
+ *
+ * [46] 全排列
+ */
+
+// @lc code=start
+func permute(nums []int) [][]int {
+	ans := make([][]int, 0)
+	option := make([]int, len(nums))
+	var backtrack func(int)
+	backtrack = func(idx int) {
+		if idx == len(nums) {
+			ans = append(ans, append([]int{}, option...))
+			return
+		}
+		for i := idx; i < len(nums); i++ {
+			option[idx] = nums[i]
+			nums[i], nums[idx] = nums[idx], nums[i]
+			backtrack(idx + 1)
+			nums[i], nums[idx] = nums[idx], nums[i]
+		}
+	}
+	backtrack(0)
+	return ans
+}
+
+// @lc code=end
