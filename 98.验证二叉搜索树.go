dff1874e0c832217879b72823ebc95ee682426f0
snowlyg <569616226@qq.com>
2022-08-01 09:35:43 +0800

    [94] 二叉树的中序遍历

---
+++ 98.验证二叉搜索树.go
@@ -0,0 +1,64 @@
+//go:build 98
+// +build 98
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	root := &TreeNode{
+		Val:  5,
+		Left: &TreeNode{Val: 1},
+		Right: &TreeNode{
+			Val:   4,
+			Left:  &TreeNode{Val: 3},
+			Right: &TreeNode{Val: 6},
+		},
+	}
+	fmt.Println(isValidBST(root))
+}
+
+/*
+ * @lc app=leetcode.cn id=98 lang=golang
+ *
+ * [98] 验证二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isValidBST(root *TreeNode) bool {
+	stack := []*TreeNode{}
+	inorder := math.MinInt64
+	for len(stack) > 0 || root != nil {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if root.Val <= inorder {
+			return false
+		}
+		inorder = root.Val
+		root = root.Right
+	}
+	return true
+}
+
+// @lc code=end
