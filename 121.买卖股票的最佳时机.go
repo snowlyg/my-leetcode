83a95f587f042fd5a406a3d3ae629d1cceb0a2d8
snowlyg <569616226@qq.com>
2022-08-05 11:32:49 +0800

    [121] 买卖股票的最佳时机

---
+++ 121.买卖股票的最佳时机.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	prices := []int{7, 1, 5, 3, 6, 4}
+	fmt.Println(maxProfit(prices))
+}
+
+/*
+ * @lc app=leetcode.cn id=121 lang=golang
+ *
+ * [121] 买卖股票的最佳时机
+ */
+
+// @lc code=start
+func maxProfit(prices []int) int {
+	// 动态规划
+	n := len(prices)
+	dp := make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, 2)
+		if i == 0 {
+			dp[i][0] = 0
+			dp[i][1] = -prices[i]
+			continue
+		}
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+		dp[i][1] = max(dp[i-1][1], -prices[i])
+	}
+	return dp[n-1][0]
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+// @lc code=end
