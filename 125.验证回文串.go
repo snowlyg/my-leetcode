fba0703efc8ec93e2239363eafa2185dd4aa6051
snowlyg <569616226@qq.com>
2022-08-05 17:44:49 +0800

    [125] 验证回文串

---
+++ 125.验证回文串.go
@@ -0,0 +1,50 @@
+//go:build 125
+// +build 125
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	s := "A man, a plan, a canal: Panama"
+	fmt.Println(isPalindrome(s))
+}
+
+/*
+ * @lc app=leetcode.cn id=125 lang=golang
+ *
+ * [125] 验证回文串
+ */
+
+// @lc code=start
+func isPalindrome(s string) bool {
+	// 双指针
+	s = strings.ToLower(s)
+	left, right := 0, len(s)-1
+	for left < right {
+		for left < right && !isalnum(s[left]) {
+			left++
+		}
+		for left < right && !isalnum(s[right]) {
+			right--
+		}
+		if left < right {
+			if s[left] != s[right] {
+				return false
+			}
+			left++
+			right--
+		}
+	}
+	return true
+}
+
+// 字符验证
+func isalnum(ch byte) bool {
+	return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')
+}
+
+// @lc code=end
