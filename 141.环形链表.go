18425176da1c4999bac5b5e64e8f657071c060ef
snowlyg <569616226@qq.com>
2022-08-08 14:18:49 +0800

    [141] 环形链表

---
+++ 141.环形链表.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	head := &ListNode{}
+	fmt.Println(hasCycle(head))
+}
+
+/*
+ * @lc app=leetcode.cn id=141 lang=golang
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
+
+// @lc code=end
